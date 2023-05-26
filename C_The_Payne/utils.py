202e65e6aead40b44c258ecc9ac5452818f46f39
Erik Solhaug <63923463+SOLHAUG13@users.noreply.github.com>
2023-05-26 14:12:21 -0500

    Enabling local loading

--- C_The_Payne/utils.py
+++ C_The_Payne/utils.py
@@ -3,22 +3,23 @@ from __future__ import absolute_import, division, print_function # python2 compa
 import numpy as np
 import os
 
-current_dir = os.getcwd()
-
-def read_in_neural_network(version="", nn_dir=current_dir): ## CHANGE TO IMPORT TLAC-SPECIFIC FILE
+def read_in_neural_network(load_local=False, nn_fname=""):
     '''
     read in the weights and biases parameterizing a particular neural network.
     You can read in existing networks from the neural_nets/ directory, or you
     can train your own networks and edit this function to read them in.
     
     ##
-    version - a string, suffix, referring to the current NN network referred to. Default is empty.
+    nn_file - a string, contains filename or filepath of a trained neural network file. F.ex: 'NN_normalized_spectra_v4.npz'
     '''
-    fname = f'NN_normalized_spectra' + version + '.npz' ##
     
-    path = os.path.join(nn_dir, fname) ##
-    tmp = np.load(path)
-    print(f'Neural network file {fname} has been read.') ##
+    if load_local==False:
+        nn_fname = os.path.join(os.path.dirname(os.path.realpath(__file__)),'other_data/NN_normalized_spectra.npz')
+    
+    tmp = np.load(nn_fname)
+    
+    print(f'Neural network file {nn_fname} has been read.')
+    
     w_array_0 = tmp["w_array_0"]
     w_array_1 = tmp["w_array_1"]
     w_array_2 = tmp["w_array_2"]
@@ -29,36 +30,49 @@ def read_in_neural_network(version="", nn_dir=current_dir): ## CHANGE TO IMPORT 
     x_max = tmp["x_max"]
     NN_coeffs = (w_array_0, w_array_1, w_array_2, b_array_0, b_array_1, b_array_2, x_min, x_max)
     tmp.close()
+    
     return NN_coeffs
 
 
-def load_wavelength_array(): ## NO NEED TO CHANGE THIS FUNCTION!
+def load_wavelength_array(load_local=False, wav_fname=""):
     '''
     read in the default wavelength grid onto which we interpolate all spectra
+    
+    load_local - a boolean, choose to load a file on your local computer
+    wav_fname - a string, contains the filename of the wavelength npz file you want to read
     '''
-    path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'other_data/TLAC_wavelength.npz') # MODIFY THIS - FLAG=0 MEANS PASS ## SPECIFY FOR ALL INSTRUMENTS
-    tmp = np.load(path)
+    
+    if load_local==False:
+        wavelength = os.path.join(os.path.dirname(os.path.realpath(__file__)),'other_data/TLAC_wavelength.npz')
+        
+    tmp = np.load(wav_fname)
     wavelength = tmp['wavelength'] # VELOCITY SPACE
     tmp.close()
+    
     return wavelength
 
 
-def load_apogee_mask(): ## NEED SPECIFIC MASK FOR EACH INSTRUMENT
+def load_apogee_mask(load_local=False, mask_fname=""): ## NEED SPECIFIC MASK FOR EACH INSTRUMENT
     '''
     read in the pixel mask with which we will omit bad pixels during spectral fitting
-    The mask is made by comparing the tuned Kurucz models to the observed spectra from Arcturus
-    and the Sun from APOGEE. We mask out pixels that show more than 2% of deviations.
+
+    load_local - a boolean, choose to load a file on your local computer
+    mask_fname - a string, contains the filename of the mask npz file you want to read
     '''
-    path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'other_data/TLAC_mask.npz') # SPECIFY FOR EACH INSTRUMENTS
-    tmp = np.load(path)
-    mask = tmp['apogee_mask'] # MODIFY ## IN VELOCITY SPACE
+    
+    if load_local==False:
+        mask_fname = os.path.join(os.path.dirname(os.path.realpath(__file__)),'other_data/TLAC_mask.npz')
+    
+    tmp = np.load(mask_fname)
+    mask = tmp['apogee_mask']
     tmp.close()
+    
     return mask
 
 
-def load_cannon_contpixels(): ## MODIFY NUMBER OF PIXELS IN CONTINUUM FILE
+def load_cannon_contpixels(): ## MODIFY NUMBER OF PIXELS IN CONTINUUM FILE # No continuum fitting needed in my case
     '''
-    read in the default list of TLAC pixels for continuum fitting.
+    read in the default list of pixels for continuum fitting.
     '''
     path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'other_data/TLAC_cont_pixels.npz') # MODIFY PIXELS
     tmp = np.load(path)
@@ -67,7 +81,7 @@ def load_cannon_contpixels(): ## MODIFY NUMBER OF PIXELS IN CONTINUUM FILE
     return pixels_cannon
 
 
-def load_training_data(version="", nn_dir=current_dir): # MODIFY NUMBER OF SPECTRA (FROM 800 TO 'NEW')
+def load_training_data(load_local=False, training_fname="", validation_fname=""): # MODIFY NUMBER OF SPECTRA (FROM 800 TO 'NEW')
     '''
     read in the default Kurucz training spectra for APOGEE
 
@@ -75,27 +89,26 @@ def load_training_data(version="", nn_dir=current_dir): # MODIFY NUMBER OF SPECT
     In practice, more training spectra will be better. The default
     neural network was trained using 12000 training spectra.
     '''
-    # TRAINING
-    fname = 'TLAC_training_spectra' + version + '.npz' ##
     
-    path = os.path.join(nn_dir, fname) ##
-    tmp = np.load(path)
-    training_labels = (tmp["labels"].T) ## Including all values here
+    if load_local==False:
+        training_fname = os.path.join(os.path.dirname(os.path.realpath(__file__)),'other_data/TLAC_training_spectra.npz')
+        validation_fname = os.path.join(os.path.dirname(os.path.realpath(__file__)),'other_data/TLAC_validation_spectra.npz')
+    
+    # TRAINING
+    tmp = np.load(training_fname)
+    training_labels = (tmp["labels"].T)
     training_spectra = tmp["spectra"]
     tmp.close()
-    
     # VALIDATION
-    fname = 'TLAC_validation_spectra' + version + '.npz' ##
-    
-    path = os.path.join(nn_dir, fname) ##
-    tmp = np.load(path)
-    validation_labels = (tmp["labels"].T) ## Including all values here
+    tmp = np.load(validation_fname)
+    validation_labels = (tmp["labels"].T)
     validation_spectra = tmp["spectra"]
     tmp.close()
+
     return training_labels, training_spectra, validation_labels, validation_spectra
 
 
-def get_apogee_continuum(spec, spec_err = None, cont_pixels = None):
+def get_apogee_continuum(spec, spec_err = None, cont_pixels = None): ### NO USE FOR MY PURPOSES AS I HAVE MY OWN FITTING CODE
     ## CHECK HOW THIS IS DIFFERENT WITH VELOCITY-SPACE SPECTRA VS. WHAT THE PAYNE USES
     ## REARRANGE THE BLUE, GREEN, RED CATEGORIES TO ONE CATEGORY FOR LY CONTINUUM
     
@@ -147,28 +160,30 @@ def _fit_cannonpixels(wav, spec, specerr, deg, cont_pixels):
     return chpoly(wav)
 
 ## I ADDED THE FUNCTIONS BELOW
-def get_loss(version=""):
+def get_loss(load_local=False, loss_fname=""):
     
-    fname = 'training_loss' + version + '.npz' ##
+    if load_local==False:
+        loss_fname = os.path.join(os.path.dirname(os.path.realpath(__file__)),'other_data/training_loss.npz')
     
-    path = os.path.join(nn_dir, fname)
+    tmp = np.load(loss_fname) # the output array also stores the training and validation loss
     
-    tmp = np.load(path) # the output array also stores the training and validation loss
-    print(f'Read training and validation loss of {path} for version {version} of C_The_Payne training.')
+    print(f'Read training and validation loss of {loss_fname}.')
+    
     training_loss = tmp["training_loss"]
     validation_loss = tmp["validation_loss"]
     tmp.close()
     
     return training_loss, validation_loss
 
-def get_validation_spectra(version=""):
+def get_validation_spectra(load_local=False, validation_fname=""):
     
-    fname = 'TLAC_validation_spectra' + version + '.npz' ##
+    if load_local==False:
+        validation_fname = os.path.join(os.path.dirname(os.path.realpath(__file__)),'other_data/TLAC_validation_spectra.npz')
     
-    path = os.path.join(nn_dir, fname)
+    tmp = np.load(validation_fname) # the output array also stores the training and validation loss
     
-    tmp = np.load(path) # the output array also stores the training and validation loss
-    print(f'Read validation spectra of {path} for version {version} of C_The_Payne training.')
+    print(f'Read validation spectra of {validation_fname}.')
+    
     spec_arr = tmp["spectra"]
     label_arr = tmp["labels"]
     tmp.close()
