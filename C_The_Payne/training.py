5c549b339c80206b1168953be97d689b975ff0c9
Erik Solhaug <63923463+SOLHAUG13@users.noreply.github.com>
2023-08-22 12:44:55 -0500

    Adding ReLU activation function to final layer

--- C_The_Payne/training.py
+++ C_The_Payne/training.py
@@ -41,6 +41,7 @@ class Payne_model(torch.nn.Module):
             torch.nn.Linear(num_neurons, num_neurons),
             torch.nn.LeakyReLU(),
             torch.nn.Linear(num_neurons, num_pixel),
+            torch.nn.LeakyReLU(), # Added ReLU in final layer
         )
 
     def forward(self, x):
